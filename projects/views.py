3587d3715d3785d7d4e9e11dcc2b3d89ab330116
Matan Shaviro <matan3sh@gmail.com>
2021-09-14 20:05:18 +0300

    first commit

---
+++ projects/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Project
+
+def projects(request):
+    projects = Project.objects.all()
+    context = { 'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': projectObj})
